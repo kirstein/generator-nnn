6de8fde875fcbbd24ec19fd4c7382b6a160299af
Mikk Kirstein <mikk@toggl.com>
2015-08-29 02:12:13 +0300

    changed jest test template

--- generators/app/templates/test/test-app.js
+++ generators/app/templates/test/test-app.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var assert = require('assert');
+var <%= camelName  %> = require('../<%= main %>');
 
 jest.dontMock('../<%= main %>');
 
 describe('<%= camelName %>', function() {
-  var <%= camelName  %> = require('../<%= main %>');
-
   it('should work', function() {
     assert.strictEqual(<%= camelName %>(), 'awesome');
   });
